c5a2ecc77c0ca678c42d0eebed7ea7a755336514
Kratosgado <mbeahessilfieprince@gmail.com>
2025-07-01 21:40:33 +0000

    add: register user dto

--- src/main/java/com/kratosgado/meeapp/security/SecurityConfig.java
+++ src/main/java/com/kratosgado/meeapp/security/SecurityConfig.java
@@ -4,7 +4,11 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -18,6 +22,24 @@ public class SecurityConfig {
 	public SecurityConfig(JwtAuthFilter jwtAuthFilter, AuthenticationProvider authProvider) {
 		this.authProvider = authProvider;
 		this.jwtAuthFilter = jwtAuthFilter;
+	}
+
+	@Bean
+	public SecurityFilterChain sFilterChain(HttpSecurity http) throws Exception {
+		return http.csrf()
+				.disable()
+				.authorizeHttpRequests()
+				.requestMatchers("auth/**")
+				.permitAll()
+				.anyRequest()
+				.authenticated()
+				.and()
+				.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and()
+				.authenticationProvider(authProvider)
+				.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+				.build();
 	}
 
 	@Bean
