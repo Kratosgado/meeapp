62cf88fe468d4c91516367fad1079bde0ccd8850
Kratosgado <mbeahessilfieprince@gmail.com>
2025-07-01 20:07:20 +0000

    add jwt dependencies

--- src/main/java/com/kratosgado/meeapp/models/User.java
+++ src/main/java/com/kratosgado/meeapp/models/User.java
@@ -1,10 +1,15 @@
 package com.kratosgado.meeapp.models;
 
 import jakarta.persistence.*;
+import java.util.Collection;
+import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -15,12 +20,55 @@ public class User {
 	@Column(nullable = false, unique = true)
 	private String email;
 
+	@Column(nullable = false)
+	private String password;
+
+	@Enumerated(EnumType.STRING)
+	private Role role;
+
 	public User() {
 	}
 
-	public User(String name, String email) {
+	public User(String name, String email, String password, Role role) {
 		this.name = name;
 		this.email = email;
+		this.password = password;
+		this.role = role;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(new SimpleGrantedAuthority(role.name()));
+	}
+
+	@Override
+	public String getPassword() {
+		throw new UnsupportedOperationException("Unimplemented method 'getPassword'");
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
 	}
 
 	public Long getId() {
@@ -46,4 +94,9 @@ public class User {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+}
+
+enum Role {
+	USER,
+	ADMIN
 }
