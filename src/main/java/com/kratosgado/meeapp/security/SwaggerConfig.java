c07b9cf80c640882c5c40f98f5b5c3f52093bbbb
Kratosgado <mbeahessilfieprince@gmail.com>
2025-07-19 15:01:49 +0000

    fix api

--- src/main/java/com/kratosgado/meeapp/security/SwaggerConfig.java
+++ src/main/java/com/kratosgado/meeapp/security/SwaggerConfig.java
@@ -2,32 +2,42 @@ package com.kratosgado.meeapp.security;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-	@Bean
-	public OpenAPI customOpenApi() {
-		return new OpenAPI()
-				.info(new Info()
-						.title("MeeApp API")
-						.version("1.0")
-						.description("API documentation for MeeApp - Group and Event Management System")
-						.contact(new Contact()
-								.name("MeeApp Team")
-								.email("support@meeapp.com")))
-				.addSecurityItem(
-						new SecurityRequirement().addList("Bearer Authentication"))
-				.components(new Components().addSecuritySchemes(
-						"Bearer Authentication", new SecurityScheme()
-								.type(SecurityScheme.Type.HTTP)
-								.scheme("bearer")
-								.bearerFormat("JWT")
-								.description("Enter JWT token")));
-	}
+  @Bean
+  public OpenAPI customOpenApi() {
+    Server server = new Server();
+    server.setUrl("http://localhost:9000");
+    server.setDescription("Development");
+
+    Contact myContact = new Contact().name("MeeApp Team").email("support@meeapp.com");
+    Info information =
+        new Info()
+            .title("MeeApp API")
+            .version("1.0")
+            .description("API documentation for MeeApp - Group and " + "Event Management System")
+            .contact(myContact);
+    return new OpenAPI()
+        .info(information)
+        .servers(List.of(server))
+        .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+        .components(
+            new Components()
+                .addSecuritySchemes(
+                    "Bearer Authentication",
+                    new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                        .description("Enter JWT token")));
+  }
 }
--- src/main/java/com/kratosgado/meeapp/utils/GlobalExceptionHandler.java
+++ src/main/java/com/kratosgado/meeapp/utils/GlobalExceptionHandler.java
@@ -3,11 +3,12 @@ package com.kratosgado.meeapp.utils;
 import com.kratosgado.meeapp.dtos.CustomResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+  
   @ExceptionHandler(ApiException.class)
   public ResponseEntity<CustomResponse> handleApiException(ApiException ex) {
     CustomResponse res = new CustomResponse(null, ex.getMessage(), ex.getStatus());
